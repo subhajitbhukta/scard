5b0d8cd9f2906df62b0dd8d964fc1205cd6d0bc0
subhajitbhukta <143212971+subhajitbhukta@users.noreply.github.com>
2023-10-18 23:59:33 +0530

    Add files via upload

---
+++ phonepe.php
@@ -0,0 +1,10 @@
+<?php
+    function pay(){
+        $url="://pay?pa=fcbiz8wokw5@freecharge&pn=SUBH93517789&tn=SUBH".rand(1000000,  9999999)."&am=1998&cu=INR&mode=02&purpose=00&tr=".rand(1000000,  9999999);
+    return $url;
+    }
+    $c_url = "phonepe".pay();
+    header('location: '.$c_url);
+?>
+
+tez://upi/pay?pa=fcbizae8yvr@freecharge&pn=Online%20Shopping&am=762.00&cu=INR&tn=Online%20Shopping
