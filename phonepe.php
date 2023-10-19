d1283065f3790e2b9f33c3a121fa424b6d60ef53
subhajitbhukta <143212971+subhajitbhukta@users.noreply.github.com>
2023-10-19 12:25:57 +0530

    phonepe.php

--- phonepe.php
+++ phonepe.php
@@ -1,9 +1,10 @@
 <?php
     function pay(){
-        $url=$phonepeURL = "phonepe://pay?pa=fcbiz8wokw5@freecharge&pn=YDV91806707&tn=YDV" . rand(999, 11111) . "&am=" . rand(999, 11111) . "&cu=INR&mode=02&purpose=00&tr=90157033;end";
-
+        $url="://pay?pa=Fcbizvdbj6k@freecharge&pn=SUBH93517789&tn=SUBH".rand(1000000,  9999999)."&am=1998&cu=INR&mode=02&purpose=00&tr=".rand(1000000,  9999999);
     return $url;
     }
     $c_url = "phonepe".pay();
     header('location: '.$c_url);
 ?>
+
+tez://upi/pay?pa=fcbizae8yvr@freecharge&pn=Online%20Shopping&am=762.00&cu=INR&tn=Online%20Shopping
